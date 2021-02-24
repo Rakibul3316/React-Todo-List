f3a625ae66657577cfdc7c3a2d7a5a419505ad45
Rakiubl-Islam-Sarker <72677836+Rakibul3316@users.noreply.github.com>
2021-02-24 20:21:02 +0600

    setup file

---
+++ src/Components/TodoInput.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TodoInput() {
+  return (
+    <div>
+      <h1>TodoInput</h1>
+    </div>
+  );
+}
---
+++ src/Components/TodoItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function TodoItem() {
+  return (
+    <div>
+      <h3>TodoItem</h3>
+    </div>
+  );
+}
